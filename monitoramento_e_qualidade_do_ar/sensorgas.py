735d7d31888d7089bd10ada40a2f1bd3e74f0baf
Thaís Lodi Pinheiro <137234283+thaisld@users.noreply.github.com>
2024-11-18 19:20:07 -0300

    comentários.py

--- monitoramento_e_qualidade_do_ar/sensorgas.py
+++ monitoramento_e_qualidade_do_ar/sensorgas.py
@@ -1,34 +1,34 @@
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+#include <LiquidCrystal_I2C.h>  // Biblioteca para controle do display LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Define o endereço I2C e o tamanho do LCD
 
-#define LED 2
-#define Buzzer 3
-#define Sensor A1
+#define LED 2  // Pino do LED
+#define Buzzer 3  // Pino do Buzzer
+#define Sensor A1  // Pino do sensor de gás
 
 void setup() {
-  Serial.begin(9200);
-  lcd.init();
-  lcd.backlight();
-  pinMode(LED, OUTPUT);
-  pinMode(Buzzer, OUTPUT);
+  Serial.begin(9200);  // Comunicação serial
+  lcd.init();  // Inicializa o LCD
+  lcd.backlight();  // Ativa a luz de fundo do LCD
+  pinMode(LED, OUTPUT);  // Configura o pino do LED como saída
+  pinMode(Buzzer, OUTPUT);  // Configura o pino do Buzzer como saída
 }
 
 void loop() {
-  int valor = analogRead(Sensor);
-  lcd.setCursor(0, 0);
-  lcd.print("Valor :");
-  lcd.print(valor);
-  lcd.print("  ");
+  int valor = analogRead(Sensor);  // Lê o valor do sensor de gás
 
-  if (valor > 400) {
-    digitalWrite(LED, HIGH);
-    digitalWrite(Buzzer, HIGH);
-    lcd.setCursor(0, 1);
-    lcd.print("Gas Detectado!");
+  lcd.setCursor(0, 0);  // Posiciona o cursor no LCD
+  lcd.print("Valor :");
+  lcd.print(valor);  // Exibe o valor lido
+
+  if (valor > 400) {  // Verifica se o valor é maior que 400
+    digitalWrite(LED, HIGH);  // Aciona o LED
+    digitalWrite(Buzzer, HIGH);  // Aciona o Buzzer
+    lcd.setCursor(0, 1);  // Move para a segunda linha do LCD
+    lcd.print("Gas Detectado!");  // Exibe mensagem de detecção de gás
   } else {
-    digitalWrite(LED, LOW);
-    digitalWrite(Buzzer, LOW);
-    lcd.setCursor(0, 1);
-    lcd.print("             ");
+    digitalWrite(LED, LOW);  // Desliga o LED
+    digitalWrite(Buzzer, LOW);  // Desliga o Buzzer
+    lcd.setCursor(0, 1);  // Move para a segunda linha do LCD
+    lcd.print("   ");  // Apaga a mensagem anterior
   }
 }
